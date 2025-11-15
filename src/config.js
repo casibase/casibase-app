58bc06aba27acc80598edfd19fd764a854ba5b24
Yang Luo <hsluoyz@gmail.com>
2025-11-15 17:28:30 +0800

    feat: improve the code

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@
 export const config = {
   // The URL of the Casibase H5 page to be embedded
   // This can be modified to power different Casibase instances
-  casibaseUrl: 'https://ai-admin.casibase.com/',
+  casibaseUrl: 'https://mini.casibase.com/',
   
   // App information
   appName: 'Casibase',
-  appDescription: 'Open-source RAG knowledge base platform'
+  appDescription: 'Casibase Operating System'
 }
 
 export default config
